594e0911987eaf1f6ff7aa84cdcfaa5767a613f4
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-11-17 14:23:49 +0900

    非同期

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'baseballs#index'
+  post 'comments', to: 'comments#create'
   resources :baseballs, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
