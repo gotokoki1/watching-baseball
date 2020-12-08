b5ad9c44417809f38d1b693a6672f6988738480c
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-12-08 15:20:11 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.public_file_server.enabled = true
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
